8cdcc7b312449a66ca8f0b9b6e526bb664d3fb0d
KimChi2893 <dtkchi2893@gmail.com>
2024-12-29 16:33:01 +0700

    format code

--- labs/lab-2/Lab_2_3.js
+++ labs/lab-2/Lab_2_3.js
@@ -6,21 +6,21 @@ let weight = readline.question('Your weight in kg: ');
 if (height > 0 && weight > 0) {
     let bmi = (weight / (height ** 2)).toFixed(1);
 
-const MIN_NORMAL_WEIGHT_BMI = 18.5;
-const MAX_NORMAL_WEIGHT_BMI = 24.9;
-const MIN_OVERWEIGHT_BMI = 25;
+    const MIN_NORMAL_WEIGHT_BMI = 18.5;
+    const MAX_NORMAL_WEIGHT_BMI = 24.9;
+    const MIN_OVERWEIGHT_BMI = 25;
 
-let deviation = 0;
+    let deviation = 0;
 
-if (bmi < MIN_NORMAL_WEIGHT_BMI) {
-    deviation = MIN_NORMAL_WEIGHT_BMI * height ** 2 - weight;
-    console.log(`You should increase ${deviation.toFixed(1)} kg`);
-} else if (bmi < MIN_OVERWEIGHT_BMI) {
-    console.log("You don't need to increase/decrease weight");
-} else {
-    deviation = Math.abs(MAX_NORMAL_WEIGHT_BMI * height ** 2 - weight);
-    console.log(`You should decrease ${deviation.toFixed(1)} kg`);
-}
+    if (bmi < MIN_NORMAL_WEIGHT_BMI) {
+        deviation = MIN_NORMAL_WEIGHT_BMI * height ** 2 - weight;
+        console.log(`You should increase ${deviation.toFixed(1)} kg`);
+    } else if (bmi < MIN_OVERWEIGHT_BMI) {
+        console.log("You don't need to increase/decrease weight");
+    } else {
+        deviation = Math.abs(MAX_NORMAL_WEIGHT_BMI * height ** 2 - weight);
+        console.log(`You should decrease ${deviation.toFixed(1)} kg`);
+    }
 } else if (height <= 0) {
     console.log("Your height should be greater than 0");
     if (weight <= 0) {
