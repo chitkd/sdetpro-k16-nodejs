e817a0bb8bbc4be4c528dbad8a4dc51e80f12730
KimChi2893 <dtkchi2893@gmail.com>
2024-12-29 16:28:43 +0700

    adjust lab 2.1 - 2.2 - 2.3 as mentor's suggestion

--- labs/lab-2/Lab_2_1.js
+++ labs/lab-2/Lab_2_1.js
@@ -2,19 +2,27 @@ const readline = require('readline-sync');
 
 let height = readline.question('Your height in m: ');
 let weight = readline.question('Your weight in kg: ');
-let bmi = (weight / (height ** 2)).toFixed(1);
+if (height > 0 && weight > 0) {
+    let bmi = (weight / (height ** 2)).toFixed(1);
 
-const MIN_NORMAL_WEIGHT_BMI = 18.5;
-const MIN_OVERWEIGHT_BMI = 25;
-const MIN_OBESITY_BMI = 30;
+    const MIN_NORMAL_WEIGHT_BMI = 18.5;
+    const MIN_OVERWEIGHT_BMI = 25;
+    const MIN_OBESITY_BMI = 30;
 
-
-if (bmi < MIN_NORMAL_WEIGHT_BMI) {
-    console.log("You're underweight");
-} else if (bmi < MIN_OVERWEIGHT_BMI) {
-    console.log("You're normal weight");
-} else if (bmi < MIN_OBESITY_BMI) {
-    console.log("You're overweight");
-} else {
-    console.log("You're obesity");
+    if (bmi < MIN_NORMAL_WEIGHT_BMI) {
+        console.log("You're underweight");
+    } else if (bmi < MIN_OVERWEIGHT_BMI) {
+        console.log("You're normal weight");
+    } else if (bmi < MIN_OBESITY_BMI) {
+        console.log("You're overweight");
+    } else {
+        console.log("You're obesity");
+    }
+} else if (height <= 0) {
+    console.log("Your height should be greater than 0");
+    if (weight <= 0) {
+        console.log("Your weight should be greater than 0");
+    }
+} else if (weight <= 0) {
+    console.log("Your weight should be greater than 0");
 }
--- labs/lab-2/Lab_2_3.js
+++ labs/lab-2/Lab_2_3.js
@@ -2,14 +2,15 @@ const readline = require('readline-sync');
 
 let height = readline.question('Your height in m: ');
 let weight = readline.question('Your weight in kg: ');
-let bmi = (weight / (height ** 2)).toFixed(1);
+
+if (height > 0 && weight > 0) {
+    let bmi = (weight / (height ** 2)).toFixed(1);
 
 const MIN_NORMAL_WEIGHT_BMI = 18.5;
 const MAX_NORMAL_WEIGHT_BMI = 24.9;
 const MIN_OVERWEIGHT_BMI = 25;
 
 let deviation = 0;
-
 
 if (bmi < MIN_NORMAL_WEIGHT_BMI) {
     deviation = MIN_NORMAL_WEIGHT_BMI * height ** 2 - weight;
@@ -19,4 +20,12 @@ if (bmi < MIN_NORMAL_WEIGHT_BMI) {
 } else {
     deviation = Math.abs(MAX_NORMAL_WEIGHT_BMI * height ** 2 - weight);
     console.log(`You should decrease ${deviation.toFixed(1)} kg`);
+}
+} else if (height <= 0) {
+    console.log("Your height should be greater than 0");
+    if (weight <= 0) {
+        console.log("Your weight should be greater than 0");
+    }
+} else if (weight <= 0) {
+    console.log("Your weight should be greater than 0");
 }
