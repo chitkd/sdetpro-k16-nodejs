c59db8d16f88c1140f88a142b6ac19b6ce0c2636
KimChi2893 <dtkchi2893@gmail.com>
2025-01-11 17:57:14 +0700

    format code

--- labs/lab-3/Lab_3_4.js
+++ labs/lab-3/Lab_3_4.js
@@ -12,10 +12,10 @@ console.log(array2);
 let isSortedMinToMaxArray1 = isSortedMinToMax(array1);
 let isSortedMinToMaxArray2 = isSortedMinToMax(array2);
 
-if (!isSortedMinToMaxArray1 || !isSortedMinToMaxArray2){
+if (!isSortedMinToMaxArray1 || !isSortedMinToMaxArray2) {
     console.log("The given array is not sorted min to max so cannot merge 2 given arrays");
-} else{
+} else {
     let resultArray = merge2SortedIntegerArray(array1, array2);
-console.log(`Expected output:`); 
-console.log(resultArray); 
+    console.log(`Expected output:`);
+    console.log(resultArray);
 }
