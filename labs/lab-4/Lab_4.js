1d4526506461634ac33d0a8f6b47d2bd49e97336
KimChi2893 <dtkchi2893@gmail.com>
2025-01-17 07:36:19 +0700

    lab 04

---
+++ labs/lab-4/Lab_4.js
@@ -0,0 +1,81 @@
+let user01 = {
+    accountNumber: "2025001",
+    routingNumber: "User_001",
+    accountName: "Nguyen Van A",
+    balance: 100000
+}
+
+// Creating another Bank Account Object which clone from the previous one and update the accountNumber only
+let user02 = JSON.parse(JSON.stringify(user01));
+user02.accountNumber = "2025002";
+
+// Put them into ann array call: bankAccounts
+let bankAccounts = [user01, user02];
+
+// Write a consolse interface app
+printConsoleInterfaceMenu();
+const readline = require('readline-sync');
+let userNumber = getUserNumber();
+switch (userNumber) {
+    case 1:
+        findAnAccount(bankAccounts);
+        break;
+    case 2:
+        updateBalance(bankAccounts);
+        break;
+    case 0:
+        break;
+    default:
+        console.log('Your task number is not on the list');
+}
+
+function printConsoleInterfaceMenu() {
+    console.log("=== Banking application ===");
+    console.log('1. Find an account\n2. Update balance\n0. Exit the program');
+}
+function getUserNumber() {
+    return Number(readline.question('Please enter your option: '))
+}
+
+function findAnAccount(bankAccounts) {
+    let accountNumber = readline.question('Please enter the account number you want to find: ');
+    let foundBankAccount = findAccountByAccountNumber(bankAccounts, accountNumber);
+
+    if (JSON.stringify(foundBankAccount) === '{}') {
+        console.log(`The bank account with account Number = '${accountNumber}' is not found`);
+    } else {
+        console.log(`The bank account information is found:\n- Account Name: ${foundBankAccount.accountName}\n- Account Balance: ${foundBankAccount.balance}`);
+    }
+}
+
+function findAccountByAccountNumber(bankAccounts, accountNumber) {
+    for (const bankAccount of bankAccounts) {
+        if (bankAccount.accountNumber === accountNumber) {
+            return bankAccount;
+        }
+    }
+    return {};
+}
+
+function updateBalance(bankAccounts) {
+    let accountNumber = readline.question('Please enter the account number you want to withdraw: ');
+    let foundBankAccount = {};
+    for (const bankAccount of bankAccounts) {
+        if (bankAccount.accountNumber === accountNumber) {
+            foundBankAccount = bankAccount;
+            let money = Number(readline.question('Please enter the money you want to withdraw: '));
+            if (bankAccount.balance < money) {
+                bankAccount.balance = 0;
+            } else {
+                bankAccount.balance -= money;
+            }
+            console.log(`The bank account after updating: ${JSON.stringify(bankAccounts)}`);
+        }
+    }
+
+    if (JSON.stringify(foundBankAccount) === '{}') {
+        console.log(`The bank account with account Number = '${accountNumber}' is not found so cannot withdraw the money`);
+    }
+}
+
+
---
+++ lessons/day-07/SwitchCaseStatement.js
@@ -0,0 +1,26 @@
+const readline = require('readline-sync');
+
+let userNum = getUserNumber();
+switch (userNum) {
+    case 2:
+
+    case 3:
+       
+    case 4:
+        
+    case 5:
+        
+    case 6:
+        console.log('Weekdays');
+        break;
+    case 7:
+    case 8:
+        console.log('Weekends');
+        break;
+    default:
+        console.log('Nhap lui roi ban oi!');
+}
+
+function getUserNumber() {
+    return Number(readline.question('Please enter your number: '))
+} 
