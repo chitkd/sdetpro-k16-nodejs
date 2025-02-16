b59133034e20bb9ac9dc3faf4deb61bc93f2f223
KimChi2893 <dtkchi2893@gmail.com>
2025-02-16 22:05:09 +0700

    day-11 & lab-6

--- labs/lab-6/9_2.js
+++ labs/lab-6/9_2.js
@@ -1,6 +1,7 @@
 const {
     apiApp
 } = require(`/Volumes/Work-Study/SOURCE_CODE/sdetpro-k16-nodejs/labs/lab-6/util/APIResponseHandler.js`);
+
 const url = 'https://jsonplaceholder.typicode.com/posts';
 let allPosts = [];
 
